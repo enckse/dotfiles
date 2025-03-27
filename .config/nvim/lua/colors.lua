b78c6fc301757fc15f5f5d05d79a29169caccddf
Sean Enck <sean@ttypty.com>
2025-03-27 17:44:18 -0400

    status line default color changed...

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -4,3 +4,5 @@ end
 
 vim.api.nvim_set_hl(0, "Pmenu", {bg='white', fg='black'})
 vim.api.nvim_set_hl(0, "Search", {bg='white', fg='black'})
+vim.api.nvim_set_hl(0, "StatusLine", {bg='white', fg='black'})
+
