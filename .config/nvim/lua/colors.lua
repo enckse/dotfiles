534f8efbce28a5aee2e0a72c4eedea9c3e8a7ee4
Sean Enck <sean@ttypty.com>
2025-03-27 18:56:06 -0400

    changing color scheme

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,8 +1,3 @@
-if vim.version().major == 0 and vim.version().minor >= 10 then
-    vim.cmd 'colorscheme vim'
-end
-
-vim.api.nvim_set_hl(0, "Pmenu", {bg='white', fg='black'})
+vim.cmd 'colorscheme wildcharm'
 vim.api.nvim_set_hl(0, "Search", {bg='white', fg='black'})
 vim.api.nvim_set_hl(0, "StatusLine", {bg='white', fg='black'})
-
