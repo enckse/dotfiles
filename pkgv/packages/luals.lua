c7dd163f0f8e5662e1a60863a9a728fe535e0997
Sean Enck <sean@ttypty.com>
2025-05-27 20:57:11 -0400

    remove wrapper script

--- pkgv/packages/luals.lua
+++ pkgv/packages/luals.lua
@@ -8,21 +8,6 @@ local module = {
     upstream = "https://github.com/LuaLS/lua-language-server"
 }
 
-local script = [[
-#!/bin/sh
-TMPPATH="/tmp/lua-language-server-$(id -u)"
-mkdir -p "$TMPPATH"
-INSTANCEPATH=$(mktemp -d "$TMPPATH/instance.XXXX")
-DEFAULT_LOGPATH="$INSTANCEPATH/log"
-DEFAULT_METAPATH="$INSTANCEPATH/meta"
-LUALS="%s"
-
-exec $LUALS/bin/lua-language-server $LUALS/main.lua \
-  --logpath="$DEFAULT_LOGPATH" \
-  --metapath="$DEFAULT_METAPATH" \
-  "$@"
-]]
-
 module.get = function(system)
     system.download(string.format("%s/releases/download/%s/lua-language-server-%s-%s-%s%s",
                                   module.upstream,
@@ -35,8 +20,7 @@ end
 
 module.build = function(system, dest, env_file)
     system.untar(module, "", dest)
-    system.create_script(dest .. "/lua-language-server", string.format(script, dest))
-    system.write_env(env_file, system.make_path_export(dest))
+    system.write_env(env_file, system.make_path_export(dest .. "/bin"))
 end
 
 return module
