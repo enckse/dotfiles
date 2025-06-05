23595620b4a5664016bd83e0141a361d689f8c37
Sean Enck <sean@ttypty.com>
2025-06-05 19:10:22 -0400

    configure exports for building

--- .config/pkgv/pkglist.lua
+++ .config/pkgv/pkglist.lua
@@ -1,5 +1,7 @@
-return {
-  {url = "https://github.com/kovidgoyal/kitty"},
-  {url = "https://github.com/rxhanson/Rectangle"},
-  {url = "https://gitlab.alpinelinux.org/alpine/aports"}
-}
+local results = {}
+if os.getenv("PKGV_DEPLOYED_OS") == "darwin" then
+  table.insert(results, {url = "https://github.com/kovidgoyal/kitty"})
+  table.insert(results, {url = "https://github.com/rxhanson/Rectangle"})
+end
+table.insert(results, {url = "https://gitlab.alpinelinux.org/alpine/aports"})
+return results
