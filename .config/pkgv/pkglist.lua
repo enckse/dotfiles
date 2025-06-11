d073f33df72ab4585bdf27129d6e533503b493e6
Sean Enck <sean@ttypty.com>
2025-06-11 10:54:31 -0400

    only execute if installed

--- .config/pkgv/pkglist.lua
+++ .config/pkgv/pkglist.lua
@@ -3,6 +3,12 @@ local filter_darwin = function(config)
 end
 return {
   {url = "https://github.com/kovidgoyal/kitty", selector = filter_darwin},
-  {url = "https://github.com/rxhanson/Rectangle", selector = filter_darwin},
+  {url = "https://github.com/rxhanson/Rectangle", selector = function(config)
+      local _, _, code = os.execute("test -d '/Applications/Rectangle.app'")
+      if code == 0 then
+          return filter_darwin(config)
+      end
+      return false
+  end},
   {url = "https://gitlab.alpinelinux.org/alpine/aports"}
 }
