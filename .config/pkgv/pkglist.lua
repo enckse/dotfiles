dd6b30cf6d667e6bc9b155e60f020b5dadd474fd
Sean Enck <sean@ttypty.com>
2025-06-12 19:28:24 -0400

    improve filter to include an app selector

--- .config/pkgv/pkglist.lua
+++ .config/pkgv/pkglist.lua
@@ -1,14 +1,14 @@
-local filter_darwin = function(config)
-    return config.os == "darwin"
+local filter_darwin = function(app)
+    return function(config)
+        local _, _, code = os.execute(string.format("test -d '/Applications/%s.app'", app))
+        if code == 0 then
+            return config.os == "darwin"
+        end
+        return false
+    end
 end
 return {
-  {url = "https://github.com/kovidgoyal/kitty", selector = filter_darwin},
-  {url = "https://github.com/rxhanson/Rectangle", selector = function(config)
-      local _, _, code = os.execute("test -d '/Applications/Rectangle.app'")
-      if code == 0 then
-          return filter_darwin(config)
-      end
-      return false
-  end},
+  {url = "https://github.com/kovidgoyal/kitty", selector = filter_darwin("kitty")},
+  {url = "https://github.com/rxhanson/Rectangle", selector = filter_darwin("Rectangle")},
   {url = "https://gitlab.alpinelinux.org/alpine/aports"}
 }
