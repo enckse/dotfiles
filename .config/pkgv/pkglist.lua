affed793091f1a3fca3034fbda2e22e937be6942
Sean Enck <sean@ttypty.com>
2025-06-14 12:01:28 -0400

    cleaning up spacing

--- .config/pkgv/pkglist.lua
+++ .config/pkgv/pkglist.lua
@@ -1,14 +1,14 @@
 local filter_darwin = function(app)
-    return function(config)
-        local _, _, code = os.execute(string.format("test -d '/Applications/%s.app'", app))
-        if code == 0 then
-            return config.os == "darwin"
-        end
-        return false
-    end
+	return function(config)
+		local _, _, code = os.execute(string.format("test -d '/Applications/%s.app'", app))
+		if code == 0 then
+			return config.os == "darwin"
+		end
+		return false
+	end
 end
 return {
-  {url = "https://github.com/kovidgoyal/kitty", selector = filter_darwin("kitty")},
-  {url = "https://github.com/rxhanson/Rectangle", selector = filter_darwin("Rectangle")},
-  {url = "https://gitlab.alpinelinux.org/alpine/aports"}
+	{ url = "https://github.com/kovidgoyal/kitty", selector = filter_darwin("kitty") },
+	{ url = "https://github.com/rxhanson/Rectangle", selector = filter_darwin("Rectangle") },
+	{ url = "https://gitlab.alpinelinux.org/alpine/aports" },
 }
