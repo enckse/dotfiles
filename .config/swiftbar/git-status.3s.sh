a5d2cb24818cf86d9f028d5979a9b5540bc8f3f6
Sean Enck <sean@ttypty.com>
2025-07-05 16:05:59 -0400

    cleaning up files

--- .config/swiftbar/git-status.3s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# <xbar.title>Git Info</xbar.title>
-# <xbar.version>v1.0</xbar.version>
-# <xbar.desc>Displays git changes</xbar.desc>
-#
-# Dependencies: none.
-
-"$HOME/.local/bin/git-uncommitted"
