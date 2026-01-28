36186d7c6ce6dda131a2a880383e536712c11d52
Sean Enck <sean@ttypty.com>
2026-01-28 17:06:20 -0500

    this can be streamlined

--- .config/devcontainer/env.sh
+++ .config/devcontainer/env.sh
@@ -5,11 +5,14 @@ export DEVCONTAINER_HOME="/home/$DEVCONTAINER_USER_NAME"
 export DEVCONTAINER_ARGS="--memory=4G"
 
 ssh_devcontainer() {
+  devcontainer start
+  IP=$(container ls --format json | jq ".[] | select(.configuration.id == \"devcontainer\")" | jq -r '.networks.[0].address' | cut -d "/" -f 1)
+  [ -z "$IP" ] && echo "no ip?" && return
   TO="$DEVCONTAINER_HOME"
   for DIR in $(echo $DEVCONTAINER_MOUNTS | tr ' ' '\n'); do
     if echo "$PWD" | grep -q "/$DIR"; then
       TO="$(echo "$PWD" | sed "s#$HOME/#$DEVCONTAINER_HOME/#g")"
     fi
   done
-  ssh -t devcontainer -- "cd '$TO'; exec bash --login"
+  ssh -t "$IP" -- "cd '$TO'; exec bash --login"
 }
